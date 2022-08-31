430e674c7592745067293fd008a8a353cd2967d0
tr0cse <gurkanakyuz75@gmail.com>
2022-08-31 17:31:47 +0300

    Initial commit

---
+++ oddoreven.js
@@ -0,0 +1,20 @@
+const oddOrEven = (num) => {
+  const len = num.toString().length;
+  let digits = 10;
+  let divide = 1;
+  let sum = 0;
+  for (let i = 1; i <= len; i++) {
+    let digit = parseInt((num % digits) / divide, 10);
+    divide *= 10;
+    digits *= 10;
+    num -= digit;
+    sum += digit;
+  }
+  if (sum % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+};
+
+console.log(oddOrEven(63));
